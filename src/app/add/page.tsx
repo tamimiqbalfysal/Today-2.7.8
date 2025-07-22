f892d7115d9539aad1b27428e95200832ab75d35
Tamim Iqbal Fysal <tamimiqbal.fysal@gmail.com>
2025-07-22 13:50:38 +0000

    change the button name from "Local" to "Global"

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -145,7 +145,7 @@ export default function AddPage() {
                             </div>
                             <div className="w-full mt-auto space-y-2">
                               <Button asChild className="w-full" variant="outline">
-                                <Link href={app.href}>Local</Link>
+                                <Link href={app.href}>Global</Link>
                               </Button>
                               <Button
                                 onClick={() => addAppToDrawer(app)}
@@ -180,7 +180,7 @@ export default function AddPage() {
                                   </div>
                                   <div className="w-full mt-auto space-y-2">
                                     <Button asChild className="w-full" variant="outline">
-                                      <Link href={app.href}>Local</Link>
+                                      <Link href={app.href}>Global</Link>
                                     </Button>
                                     <Button
                                       onClick={() => addAppToDrawer(app)}
